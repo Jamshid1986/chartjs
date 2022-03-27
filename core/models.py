21a487821174e4bc644aa47cf6d699117159853e
Jamshid1986 <jimamaliyev@gmail.com>
2022-03-27 18:07:05 +0500

    added verbose name

---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    matni = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Postlar"
+     
