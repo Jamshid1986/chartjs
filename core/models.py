25b996dc26d4f3a92268a4c3e4a5292e4974ba34
Jamshid1986 <jimamaliyev@gmail.com>
2022-03-27 18:51:49 +0500

    contextni templatega ulash

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Post(models.Model):
     name = models.CharField(max_length=50)
     matni = models.TextField()
+    daromadi = models.IntegerField()
 
     def __str__(self):
         return self.name
--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+def example(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, "examp.html", context)
